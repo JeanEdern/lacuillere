7651977ee6671eed01dfd605735b7281eca4edb9
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-02-11 13:31:50 +0100

    la cuillere done

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ActiveRecord::Base
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates_numericality_of :rating, :on => :create
+  validates :restaurant_id, presence: true
+  validates :rating, presence: true, inclusion: { in: [0,1,2,3,4,5], allow_nil: false }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "01 53 43 43 40",
+    category:     "French"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "01 46 33 29 82",
+    category:     "French"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "01 44 14 41 00",
+    category:     "Spanish"
+  },
+
+  {
+    name:         "Okuda",
+    address:      "7, rue de la Trémoille, 75008",
+    phone_number: "01 40 70 19 19",
+    category:     "Japanese"
+  },
+
+   {
+    name:         "Al Ajami Paris",
+    address:      "58 Rue François 1er, 75008",
+    phone_number: "01 42 25 38 44",
+    category:     "Lebanese"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }

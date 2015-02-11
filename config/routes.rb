e14a390ce0d8f589114dd40e3a90309ce1b40505
Jean-Edern Lorillon <trashjelo@gmail.com>
2015-02-11 19:32:57 +0100

    exo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root :to => "restaurants#index"
 
   resources :restaurants do
     resources :reviews, only: [:new, :create, :show]
   end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
